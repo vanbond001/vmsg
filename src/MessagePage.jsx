034e0f9f1f3ad02a566e5d947f95dea0e04a787b
vanbond001 <157499591+vanbond001@users.noreply.github.com>
2024-01-29 18:59:22 +0530

    Update MessagePage.jsx (wss)

--- src/MessagePage.jsx
+++ src/MessagePage.jsx
@@ -57,7 +57,7 @@ const handleClose = (messageId) => {
   }, [selectedUserId]);
   function connectToWs() {
     // const ws = new WebSocket('ws://localhost:4040');
-    const ws = new WebSocket('ws://vchat-backend-cs72.onrender.com');
+    const ws = new WebSocket('wss://vchat-backend-cs72.onrender.com');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
