3a241443c0dfb52a5cea75a7c1efedc2e18e8797
vanbond001 <157499591+vanbond001@users.noreply.github.com>
2024-02-23 16:25:55 +0530

    Update main.jsx1
    testing with hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/c1'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter } from 'react-router-dom';  // Change to HashRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
+    <HashRouter>
+      <div>
+      <App />
+        <ToastContainer />
+         </div>
+    </HashRouter>
+    {/* // </BrowserRouter>  */}
+  </React.StrictMode>
   </React.StrictMode>,
 )
